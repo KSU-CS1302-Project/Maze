4beccd9734435bb6188dea213f79455ce7c0ef4f
grantw99 <35930267+grantw99@users.noreply.github.com>
2018-02-05 13:38:53 -0500

    Test, removed e

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -9,7 +9,7 @@ package maze;
 
 public class Maze {
 
-    //n, s, w, e
+    //n, s, w
     private char direction;
     private int r;  // x position of the mouse
     private int c;  //y position of the mouse
